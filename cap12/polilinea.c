7ef2e253bb38a559b0257625eb8c9e579db82d85
shiguera <shiguera@telefonica.net>
2025-08-10 15:06:38 +0200

    Update Chapter 12, polilinea.c

--- cap12/polilinea.c
+++ cap12/polilinea.c
@@ -18,6 +18,12 @@ double longitud(PoliLinea plinea);
 void mostrar(PoliLinea plinea);
 void eliminar(PoliLinea* plinea, int index);
 void simplificar(PoliLinea* plinea, int index);
+PoliLinea simplificar_cow(PoliLinea* plinea, int index);
+void anadir_puntos(PoliLinea* plinea, Punto* puntos, int num_puntos);
+PoliLinea unir(PoliLinea plinea1, PoliLinea plinea2 );
+bool es_cerrada(PoliLinea plinea);
+PoliLinea cerrar(PoliLinea plinea);
+
 
 int main() {
     PoliLinea plinea = {0};
@@ -28,9 +34,30 @@ int main() {
     
     mostrar(plinea);
     // eliminar(&plinea, 2); // Puedes probar la función eliminar
-    eliminar(&plinea, 2);
+    simplificar(&plinea, 2);
     mostrar(plinea);
-    
+
+    // Ej. 7
+    Punto puntos[3] = { {1.0, 2.0}, {2.5, 3.6}, {-1.2, 3.14}};
+    anadir_puntos(&plinea, puntos, 3);
+
+    // Ej 8
+    PoliLinea plinea2 = {0};
+    anadir_vertice(&plinea, (Punto){3.0, -2.0});
+    anadir_vertice(&plinea, (Punto){1.5, 4.0});
+    anadir_vertice(&plinea, (Punto){6.0, -3.0});
+    PoliLinea linresult = unir(plinea, plinea2);
+    mostrar(linresult);
+
+    // Ej. 9
+    PoliLinea linresult_cow = simplificar_cow(&linresult, linresult.num_vertices-1);
+    mostrar(linresult_cow);
+
+    // Ej. 10 y 11
+    printf("No debe ser cerrada: %s\n", es_cerrada(plinea2)? "SI": "NO");
+    PoliLinea lincerrada = cerrar(plinea2);
+    printf("Sí debe ser cerrada: %s\n", es_cerrada(lincerrada)? "SI": "NO");
+
     return 0;
 }
 
@@ -78,4 +105,50 @@ void simplificar(PoliLinea* plinea, int index) {
         plinea->vertices[index+i] = plinea->vertices[index+i+1];
     }
     plinea->num_vertices += -1;
+}
+
+PoliLinea simplificar_cow(PoliLinea* plinea, int index) {
+    PoliLinea linresult = {0};
+    for(int i=0; i<index; i++) {
+        anadir_vertice(&linresult, plinea->vertices[i]);
+    }
+    if((index+1) < plinea->num_vertices) {
+        for(int i=index+1; i<plinea->num_vertices; i++) {
+            anadir_vertice(&linresult, plinea->vertices[i]);
+        }   
+    }
+    return linresult;
+}
+
+void anadir_puntos(PoliLinea* plinea, Punto* puntos, int num_puntos) {
+    for(int i=0; i<num_puntos; i++) {
+        anadir_vertice(plinea, puntos[i]);
+    }
+}
+
+PoliLinea unir(PoliLinea plinea1, PoliLinea plinea2 ) {
+    PoliLinea linresult ={0};
+    for(int i=0; i<plinea1.num_vertices; i++) {
+        anadir_vertice(&linresult, plinea1.vertices[i]);
+    }
+    for(int i=0; i<plinea2.num_vertices; i++) {
+        anadir_vertice(&linresult, plinea2.vertices[i]);
+    }
+    return linresult;
+}
+
+bool es_cerrada(PoliLinea plinea) {
+    bool result = false;
+    Punto p0 = plinea.vertices[0];
+    Punto pult = plinea.vertices[plinea.num_vertices-1];
+    if( (p0.x==pult.x) && (p0.y==pult.y)) {
+        result = true;
+    }
+    return result;
+}
+
+PoliLinea cerrar(PoliLinea plinea) {
+    PoliLinea linresult = plinea;
+    anadir_vertice(&linresult, plinea.vertices[plinea.num_vertices-1]);
+    return linresult;
 }
